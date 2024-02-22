60e8c2fc3f9ab68f7f8c5e8c46d258a8029fabe5
Joshua Edwards <inaruslynx@gmail.com>
2024-02-22 04:28:07 -0600

    Init

---
+++ sdi-walkthrough/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import NavBar from "@/components/nav-bar";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "SDI Walkthrough",
+  description: "A walkthrough app for all SDI divisions",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <NavBar />
+        {children}
+      </body>
+    </html>
+  );
+}
