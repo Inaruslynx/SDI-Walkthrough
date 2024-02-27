147068fa3dd66fd6f7aa4f391512a57c61cc56b9
Joshua Edwards <inaruslynx@gmail.com>
2024-02-27 12:23:23 -0600

    update .gitignore and dbConnect.ts

--- sdi-walkthrough/Next Front-end/utils/dbConnect.ts
+++ sdi-walkthrough/Next Front-end/utils/dbConnect.ts
@@ -1,3 +1,5 @@
+// ! This file won't be necessary using Express as back-end
+
 import mongoose from "mongoose";
 declare global {
   var mongoose: any; // This must be a `var` and not a `let / const`
