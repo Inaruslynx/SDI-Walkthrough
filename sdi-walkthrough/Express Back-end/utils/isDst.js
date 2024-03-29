ae47c6e5c641e15af8d403ce947b09d6f118a3f2
Inaruslynx <inaruslynx@gmail.com>
2024-03-28 19:16:51 -0500

    Fixed isDst

--- sdi-walkthrough/Express Back-end/utils/isDst.js
+++ sdi-walkthrough/Express Back-end/utils/isDst.js
@@ -1,11 +1,22 @@
-module.exports = function isDst(date) {
-  const year = date.getUTCFullYear();
-  const marchSecondSunday = new Date(
-    Date.UTC(year, 2, 8 - new Date(Date.UTC(year, 2, 1)).getUTCDay())
-  );
-  const novemberFirstSunday = new Date(
-    Date.UTC(year, 10, 1 - new Date(Date.UTC(year, 10, 1)).getUTCDay())
-  );
+const { startOfMonth, nextSunday, isAfter, isBefore } = require("date-fns");
 
-  return date >= marchSecondSunday && date < novemberFirstSunday;
+module.exports = function isDst(date) {
+  const result =
+    isAfter(date, getSecondSundayInMarch()) &&
+    isBefore(date, getFirstSundayInNovember());
+
+  return result;
 };
+
+function getSecondSundayInMarch() {
+  const marchFirst = startOfMonth(new Date().getFullYear(), 2, 1);
+  const firstSunday = nextSunday(marchFirst);
+  const secondSunday = nextSunday(firstSunday);
+  return secondSunday;
+}
+
+function getFirstSundayInNovember() {
+  const novFirst = startOfMonth(new Date().getFullYear(), 10, 1);
+  const firstSunday = nextSunday(novFirst);
+  return firstSunday;
+}
