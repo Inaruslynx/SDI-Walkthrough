06dbf252ba9c3011479fd6085bfcbfb568a07b77
Inaruslynx <inaruslynx@gmail.com>
2024-08-24 13:47:24 -0500

    Updated front page

--- sdi-walkthrough/Next Front-end/app/page.tsx
+++ sdi-walkthrough/Next Front-end/app/page.tsx
@@ -5,19 +5,18 @@ export default function Home() {
         <div className="container">
           <h1>Welcome to SDI Walkthrough</h1>
           <p>
-            This is a very early version of the new site. I am utilizing new
-            technology: Next.js which is a server that uses React seamlessly on
-            client and server.
+            This app should be pretty fleshed out, but bugs might still exist. Please let me know if you find any issues. I am utilizing new
+            technology: Next.js and Nest.js.
           </p>
           <h3>TODO List:</h3>
           <ul className="px-6">
             <li>
-              <s>Use Nestjs Back-end for data.</s>
+              <s>Use Nest.js Back-end for data.</s>
             </li>
             <li>
               <s>
                 Change navbar based on available walkthroughs like Elec, Mech,
-                and Ops. Can get data from Express.
+                and Ops.
               </s>
             </li>
             <li>
@@ -26,25 +25,15 @@ export default function Home() {
             <li>
               <s>Departments can have multiple walkthroughs.</s>
             </li>
-            <li>Forms will have form validation and show a small graph.</li>
+            <li>Forms will have form validation.</li>
+            <li>Forms will show a small graph.</li>
             <li>
               <s>Graph page.</s>
             </li>
             <li>
               <s>Ability for admins to configure walkthroughs.</s>
             </li>
-            <li>
-              <s>
-                Connect to Microsoft identities so users log in with Microsoft
-                account. Either use Clerk or something on Express. (maybe)
-              </s>
-            </li>
-            <li>
-              <s>
-                Leverage Microsoft identities to know what department a user is
-                part of and if they are an admin. (maybe)
-              </s>
-            </li>
+            <li>Admins can configure user accounts.</li>
             <li>
               Periodicity of walkthroughs:
               <ul>
